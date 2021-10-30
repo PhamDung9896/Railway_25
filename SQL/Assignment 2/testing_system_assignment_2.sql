024ebce013809ae145f22093d257124bbad02db4
PhamDung9896 <dung41383@gmail.com>
2021-10-30 22:52:48 +0900

    Update 1,2 and submit 3

--- SQL/Assignment 2/testing_system_assignment_2.sql
+++ SQL/Assignment 2/testing_system_assignment_2.sql
@@ -1,7 +1,6 @@
-DROP DATABASE IF EXISTS testing_system_assignment_1;
-CREATE DATABASE testing_system_assignment_1;
-USE testing_system_assignment_1;
-
+DROP DATABASE IF EXISTS testing_system_assignment_2;
+CREATE DATABASE testing_system_assignment_2;
+USE testing_system_assignment_2;
 -- tai sao chay chung thi ok ma chay rieng thi bao loi????--
 								
 												-- ***** --
@@ -22,21 +21,21 @@ CREATE TABLE `Position`(
 
 -- Tao Table 3: Account
 CREATE TABLE `Account`(
-			AccountID		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-			Email			VARCHAR(50) UNIQUE NOT NULL,
-			Username		VARCHAR(20) UNIQUE NOT NULL CHECK(length(Username)>=6) ,
-			FullName		NVARCHAR(30) NOT NULL,
-			DepartmentID	TINYINT UNSIGNED DEFAULT 1, -- khi nhan vien moi vao cty chua dc phan vao phong ban nao thi se dc vao phong ban so 1
-			PositionID		TINYINT UNSIGNED DEFAULT 1, -- tuong tu nhu DepartmentID
-			CreateDate		DATETIME DEFAULT NOW() -- mac dinh la thoi diem ngay luc du lieu dc nhap vao va chay cau lenh
+			AccountID			TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			Email				VARCHAR(50) UNIQUE NOT NULL,
+			Username			VARCHAR(20) UNIQUE NOT NULL CHECK(length(Username)>=3) ,
+			FullName			NVARCHAR(30) NOT NULL,
+			DepartmentID		TINYINT UNSIGNED DEFAULT 1, -- khi nhan vien moi vao cty chua dc phan vao phong ban nao thi se dc vao phong ban so 1
+			PositionID			TINYINT UNSIGNED DEFAULT 1, -- tuong tu nhu DepartmentID
+			CreateDate			DATETIME DEFAULT NOW() -- mac dinh la thoi diem ngay luc du lieu dc nhap vao va chay cau lenh
 );
 
 -- Tao Table 4: Group
 CREATE TABLE `Group`(
-			GroupID			TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-			GroupName		VARCHAR(30) UNIQUE NOT NULL,
-			CreatorID		TINYINT UNSIGNED NOT NULL DEFAULT 1,
-			CreateDate		DATETIME DEFAULT NOW()
+			GroupID				TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+			GroupName			VARCHAR(30) UNIQUE NOT NULL,
+			CreatorID			TINYINT UNSIGNED NOT NULL DEFAULT 1,
+			CreateDate			DATETIME DEFAULT NOW()
 );
 
 -- Tao Table 5: GroupAccount
@@ -52,21 +51,21 @@ CREATE TABLE GroupAccount(
 -- Tao Table 6: TypeQuestion
 CREATE TABLE TypeQuestion(
 			TypeID			TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-			TypeName		ENUM('ESSAY','MULTIPLE-CHOICE') NOT NULL DEFAULT 'ESSAY'
+			TypeName		ENUM('ESSAY','MULTIPLE_CHOICE') NOT NULL DEFAULT 'ESSAY'
 );
 
 -- Tao Table 7: CategoryQuestion
 CREATE TABLE CategoryQuestion(
 			CategoryID		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-			CategoryName	VARCHAR(50) UNIQUE NOT NULL CHECK(length(CategoryName)>=3)
+			CategoryName	VARCHAR(50) UNIQUE NOT NULL CHECK(length(CategoryName)>=1)
 );
 
 -- Tao Table 8: Question
 CREATE TABLE Question(
 			QuestionID		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-			Content			VARCHAR(150) UNIQUE NOT NULL CHECK(length(Content)>=10),
+			Content			VARCHAR(150) UNIQUE NOT NULL CHECK(length(Content)>=5),
 			CategoryID		TINYINT UNSIGNED NOT NULL DEFAULT 1,
-			TypeID			TINYINT UNSIGNED NOT NULL DEFAULT 1, -- tao sao dat chu ko dc???
+			TypeID			TINYINT UNSIGNED NOT NULL DEFAULT 1, 
 			CreatorID		TINYINT UNSIGNED NOT NULL DEFAULT 1,
 			CreateDate		DATETIME DEFAULT NOW(),
             FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion(CategoryID),
@@ -76,7 +75,7 @@ CREATE TABLE Question(
 -- Tao Table 9: Answer
 CREATE TABLE Answer(
 			AnswerID		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-			Content			VARCHAR(150) UNIQUE NOT NULL CHECK(length(Content)>=3),
+			Content			VARCHAR(150) UNIQUE NOT NULL CHECK(length(Content)>=2),
 			QuestionID		TINYINT UNSIGNED NOT NULL,
 			isCorrect		ENUM('TRUE','FALSE') NOT NULL,
             FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
@@ -104,83 +103,188 @@ CREATE TABLE ExamQuestion(
 
 													-- ***** --
 -- Question 3: Chuẩn bị data cho bài 3: Insert data vào 11 table, mỗi table có ít nhất 5 records
-INSERT INTO Department(DepartmentID,DepartmentName)
-VALUES				('1','A1'),
-					('2','A2'),
-                    ('3','A3'),
-                    ('4','A4'),
-                    ('5','A5');
+-- testing_system_assignment_3--
+-- Question 1: Thêm ít nhất 10 record vào mỗi table
+INSERT INTO Department(DepartmentName)
+VALUES				('S1'),
+					('S2'),
+					('S3'),
+                    ('S4'),
+                    ('S5'),
+                    ('S6'),
+                    ('S7'),
+                    ('S8'),
+                    ('S9'),
+                    ('S10'),
+                    ('S11'),
+                    ('S12'),
+                    ('S13'),
+                    ('S14'),
+                    ('S15');
                     
-INSERT INTO `Position`(PositionID,PositionName)
-VALUES				('1','Software_Engineer'),
-					('2','Computer_Programmer'),
-                    ('3','Computer_System_Analysist'),
-                    ('4','Web_Developers'),
-                    ('5','Software_Testers');
+INSERT INTO `Position`(PositionName)
+VALUES				('Software Engineer'),
+					('Computer Programmer'),
+                    ('Computer System Analysist'),
+                    ('Web Developers'),
+                    ('Software Testers'),
+                    ('Database Administrator'),
+                    ('Computer Network Architects'),
+                    ('Quality Assurance Engineers'),
+                    ('Front End Developer'),
+                    ('Data Quality Manager'),
+                    ('Help Desk Specialist'),
+                    ('IT Support Manager'),
+                    ('Java Developer'),
+                    ('Information System Security Staff'),
+                    ('Web Desinger');
+                    
 
 				
-INSERT INTO `Account`(AccountID,Emai,Username,FullName,DepartmentID,PositionID,CreateDate)
-VALUES				('1','LETHUANDUC@gmail.com','LeDuc','LeThuanDuc','1','1','2019/03/02'),
-					('2','VOHOHONGLOAN@gmail.com','VoLoan','VoHoHongLoan','1','5','2018/05/12'),
-                    ('3','NGUYENNGOCHUY@gmail.com','NguyenHuy','NguyenNgocHuy','3','4','2019/12/09'),
-                    ('4','VOKHOIHONGNHUNG@gmail.com','VoNhung','VoKhoiHongNhung','3','3','2017/01/30'),
-                    ('5','DANGHONHATTHACH@gmail.com','DangThach','DangHoNhatThach','2','2','2019/12/03'),
-                    ('6','VOVANTHANG@gmail.com','VoThang','VoVanThang','4','3','2020/01/01');
+INSERT INTO `Account`(Email,Username,FullName,DepartmentID,PositionID,CreateDate)
+VALUES				('LETHUANDUC@gmail.com','LeDuc','LeThuanDuc',1,3,'2019/03/12'),
+					('VOHOHONGLOAN@gmail.com','VoLoan','VoHoHongLoan',2,2,'2018/05/12'),
+                    ('NGUYENNGOCHUY@gmail.com','NguyenHuy','NguyenNgocHuy',3,1,'2019/12/09'),
+                    ('VOKHOIHONGNHUNG@gmail.com','VoNhung','VoKhoiHongNhung',1,3,'2017/01/30'),
+                    ('DANGHONHATTHACH@gmail.com','DangThach','DangHoNhatThach',1,1,'2019/12/03'),
+                    ('VOVANTHANG@gmail.com','VoThang','VoVanThang',2,3,'2020/01/01'),
+                    ('HOANGMINHTIEN@gmail.com','HoangTien','HoangMinhTien',3,1,'2019/07/02'),
+                    ('DANGHOAIAN@gmail.com','DangAn','DangHoaiAn',3,4,'2019/03/16'),
+                    ('BUITHIHIENNA@gmail.com','BuiNa','BuiThiHienNa',4,2,'2019/08/09'),
+                    ('LEPHANMINHQUAN@gmail.com','LeQuan','LePhanMinhQuan',4,1,'2019/09/25'),
+                    ('PHAMTHINGOCTRANG@gmail.com','PhamTrang','PhamThiNgocTrang',5,1,'2019/05/05'),
+                    ('VANTHUTRUNG@gmail.com','VanTrung','VanThuTrung',3,4,'2019/10/02'),
+                    ('DINHTHANHTUONGVY@gmail.com','DinhVy','DinhThanhTuongVy',3,2,'2019/11/02'),
+                    ('PHANTHAIBACH@gmail.com','PhanBach','PhanThaibach',4,1,'2019/03/30'),
+                    ('LEHOANGBINH@gmail.com','LeBinh','LeHoangBinh',4,3,'2019/05/02');
+                    
 
 
-INSERT INTO  `Group`(GroupID,GroupName,CreatorID,CreateDate)
-VALUES				('1','B1','1','2019/05/13'),
-					('2','B2','3','2020/01/02'),
-                    ('3','B3','5','2019/12/15'),
-                    ('4','B4','7','2019/12/9'),
-                    ('5','B5','9','2019/11/31');
+INSERT INTO  `Group`(GroupName,CreatorID,CreateDate)
+VALUES				('A',1,'2019/03/12'),
+					('B',3,'2018/05/12'),
+                    ('C',2,'2019/12/09'),
+					('D',4,'2017/01/30'),
+                    ('E',5,'2019/12/03'),
+                    ('F',4,'2020/01/01'),
+					('J',2,'2019/07/02'),
+                    ('K',3,'2019/03/16'),
+                    ('L',2,'2019/08/09'),
+                    ('M',5,'2019/09/25'),
+                    ('N',6,'2019/05/05'),
+                    ('O',2,'2019/10/02'),
+                    ('S',3,'2019/11/02'),
+                    ('G',2,'2019/03/30'),
+                    ('W',1,'2019/05/02');
 
 
 INSERT INTO GroupAccount(GroupID,AccountID,JoinDate)
-VALUES				('1','VIT1','2020/12/03'),
-					('2','VIT2','2019/12/30'),
-					('3','VIT3','2019/11/22'),
-					('4','VIT4','2020/05/12'),
-					('5','VIT5','2020/03/25');
+VALUES				(1,2,'2020/12/03'),
+					(2,3,'2019/12/30'),
+					(3,4,'2019/11/22'),
+					(4,5,'2020/05/12'),
+					(5,6,'2020/03/25'),
+                    (6,7,'2020/12/03'),
+                    (7,1,'2020/12/07'),
+                    (7,2,'2020/12/08'),
+                    (3,5,'2020/12/07'),
+                    (1,5,'2019/10/03'),
+                    (6,2,'2020/11/03'),
+                    (2,4,'2020/10/03'),
+                    (2,1,'2019/01/03'),
+                    (3,2,'2020/12/03'),
+                    (1,6,'2020/12/01');
+
+                   
 
 
-INSERT INTO TypeQuestion(TypeID,TypeName)
-VALUES				('1','ESSAY'),
-					('2','MULTIPLE_CHOICE'),
-					('3','MULTIPLE_CHOICE'),
-					('4','ESSAY'),
-					('5','MULTIPLE_CHOICE');
+INSERT INTO TypeQuestion(TypeName)
+VALUES				('ESSAY'),
+					('MULTIPLE_CHOICE'),
+					('MULTIPLE_CHOICE'),
+					('ESSAY'),
+					('MULTIPLE_CHOICE'),
+                    ('ESSAY'),
+                    ('ESSAY'),
+                    ('ESSAY'),
+                    ('MULTIPLE_CHOICE'),
+                    ('MULTIPLE_CHOICE'),
+                    ('ESSAY'),
+                    ('ESSAY'),
+                    ('ESSAY'),
+                    ('MULTIPLE_CHOICE'),
+                    ('ESSAY');
 
 
-INSERT INTO  CategoryQuestion(CategoryID,CategoryName)
-VALUES				('1','Java'),
-					('2','.NET'),
-					('3','SQL'),
-					('4','Postman'),
-					('5','Ruby');
+INSERT INTO  CategoryQuestion(CategoryName)
+VALUES				('Java'),
+					('.NET'),
+					('SQL'),
+					('Postman'),
+					('Ruby'),
+                    ('Python'),
+                    ('PHP'),
+                    ('Swift'),
+                    ('C#'),
+                    ('Objective-C'),
+                    ('C++'),
+                    ('C'),
+                    ('Pascal'),
+                    ('JavaScript'),
+                    ('HTML');
                     
-INSERT INTO  Question(QuestionID,Content,CategoryID,TypeID,CreatorID,CreateDate)
-VALUES				('1','Java la gi','2','2','1','2020/12/01'),
-					('2','Tai sao Java lai duoc ua chuong','1','4','5','2019/02/01'),
-                    ('3','Su khac nhau giua .NET va Java','3','4','5','2019/03/01'),
-                    ('4','Dung SQL de lam gi','6','5','4','2019/05/12'),
-                    ('5','Su khac nhau giua SQl va Excel','3','3','4','2019/12/25');
+INSERT INTO  Question(Content,CreateDate)
+VALUES				('Java la gi','2020/12/01'),
+					('Tai sao Java lai duoc ua chuong','2019/02/01'),
+                    ('Su khac nhau giua .NET va Java','2019/03/01'),
+                    ('Dung SQL de lam gi','2019/05/12'),
+                    ('Su khac nhau giua SQl va Excel','2019/12/25'),
+                    ('1byte bang bao nhieu bit','2020/12/01'),
+                    ('SQL la gi','2020/11/01'),
+                    ('Tai sao nen dung SOL','2019/12/01'),
+                    ('su khac nhau giua SQL va Ruby','2020/12/21'),
+                    ('khi nao dung Java','2019/02/01'),
+                    ('C++ la gi','2019/12/03'),
+                    ('Ruby la gi','2020/10/01'),
+                    ('Python la gi','2019/12/01'),
+                    ('.NET la gi','2020/09/01'),
+                    ('C# la gi','2020/08/01');
 
 
-INSERT INTO Answer(AnswerID,QuestionID,isCorrect)
-VALUES				('1','2','TRUE'),
-					('2','3','TRUE'),
-                    ('3','4','FALSE'),
-                    ('4','5','FALSE'),
-                    ('5','1','FALSE');
+INSERT INTO Answer(Content,QuestionID,isCorrect)
+VALUES				('11',1,'TRUE'),
+					('21',3,'TRUE'),
+                    ('31',2,'FALSE'),
+                    ('42',5,'FALSE'),
+                    ('52',6,'FALSE'),
+                    ('62',7,'TRUE'),
+                    ('74',8,'TRUE'),
+                    ('84',11,'FALSE'),
+                    ('94',10,'TRUE'),
+                    ('10',12,'FALSE'),
+                    ('19',13,'TRUE'),
+                    ('12',14,'TRUE'),
+                    ('13',1,'FALSE'),
+                    ('14',2,'TRUE'),
+                    ('15',3,'TRUE');
 
 
-INSERT INTO Exam(ExamID,`Code`,CategoryID,Duration)
-VALUES				('1','FE1','2','45'),	
-					('2','FE2','2','90'),
-					('3','FE1','3','30'),
-					('4','FE2','4','45'),
-					('5','FE2','5','90');
+INSERT INTO Exam(`Code`,Title,CategoryID,CreatorID,Duration)
+VALUES				('FE1','AVG',2,'2','45'),	
+					('FE2','BINARY',3,'3','90'),
+					('FE3','CASE','1',4,'30'),
+					('FE4','DEFAULT',1,'5','45'),
+					('FE5','EXISTS',2,'4','90'),
+                    ('FE6','fresher',3,'2','45'),
+                    ('FE7','GROUPING',4,'1','45'),
+                    ('FE8','JSON_ARRAYAGG',5,'2','45'),
+                    ('FE9','LAG',6,'3','90'),
+                    ('FE10','RANK',7,'2','45'),
+                    ('FE11','TIMESTAMP',4,'1','30'),
+                    ('FE22','TRUE','5',4,'45'),
+                    ('FE13','PERCENT_RANK','2',3,'90'),
+                    ('FE14','CONVERT','2',6,'45'),
+                    ('FE15','STDDEV','1',7,'45');
 
 
 INSERT INTO ExamQuestion(ExamID,QuestionID)
@@ -188,8 +292,17 @@ VALUES				('2','3'),
 					('3','3'),
 					('4','4'),
 					('4','1'),
-					('5','2');
-
+					('5','2'),
+                    ('2','3'),
+                    ('2','3'),
+                    ('2','4'),
+                    ('2','4'),
+                    ('5','3'),
+                    ('5','2'),
+                    ('2','1'),
+                    ('3','3'),
+                    ('4','3'),
+                    ('2','3');
 
 
 
---
+++ SQL/Assignment 3/testing_system_assignment_3.sql
@@ -0,0 +1,94 @@
+DROP DATABASE IF EXISTS testing_system_assignment_3;
+CREATE DATABASE testing_system_assignment_3;
+USE testing_system_assignment_2;
+
+#NOTES:
+-- Nhung du lieu de khoa PRIMARY KEY AUTO_INCREMENT thi ko can INSERT nua
+-- Nhung loi da gap Error Code: 1062. Duplicate entry 'A1' for key 'department.DepartmentName'=>????
+-- 					Error Code: 1136. Column count doesn't match value count at row 1=>check lai cac du lieu
+-- 					Error Code: 1049. Unknown database 'testing_system_assignment_2'=>phai chay lai thu cac cau lenh
+-- 					Error Code: 1146. Table 'testing_system_assignment_2.department' doesn't exist=>phai chay lai thu cac cau lenh
+-- 					Error Code: 1050. Table 'position' already exists=>????
+-- 					Error Code: 1062. Duplicate entry 'Help_Desk_Specialist' for key 'position.PositionName'
+-- 					Error Code: 3819. Check constraint 'account_chk_1' is violated.
+-- 					Error Code: 1364. Field 'GroupID' doesn't have a default value
+
+
+
+-- Chu y su dong nhat giua du lieu cu va du lieu moi, neu trong truong hop muon dung lai du lieu cua bt truoc
+              
+                    
+-- Question 2: lấy ra tất cả các phòng ban
+SELECT *
+FROM Department;
+
+-- Question 3: lấy ra id của phòng ban "Sale"
+SELECT DepartmentID
+FROM Department
+WHERE DepartmentName='Sale';
+
+-- Question 4: lấy ra thông tin account có full name dài nhất
+SELECT FullName,length(FullName) AS do_dai_ten
+FROM `Account`
+WHERE length(FullName)= (SELECT max(length(FullName)) FROM `account`);
+						
+
+-- Question 5: Lấy ra thông tin account có full name dài nhất và thuộc phòng ban có id = 3
+SELECT 		DepartmentID,FullName,length(FullName) AS do_dai_ten
+FROM 		`Account`
+WHERE 		DepartmentID=3 AND length(FullName)= (SELECT max(length(FullName))
+												  FROM `account`
+                                                  WHERE DepartmentID=3);
+
+-- Question 6: Lấy ra tên group đã tham gia trước ngày 20/12/2019
+SELECT		GroupName
+FROM		`Group`
+WHERE		CreateDate <'2019/12/20';
+
+-- Question 7: Lấy ra ID của question có >= 4 câu trả lời
+SELECT		QuestionID, count(QuestionID) AS So_luong
+FROM		Answer
+GROUP BY	QuestionID
+HAVING 		count(QuestionID)>=4;
+
+-- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút và được tạo trước ngày 20/12/2019
+SELECT		`Code`
+FROM		Exam
+WHERE		Duration>=60 AND CreateDate<'2019/12/20';
+
+-- Question 9: Lấy ra 5 group được tạo gần đây nhất
+SELECT		*
+FROM		`Group`
+ORDER BY	CreateDate DESC
+LIMIT		5;
+
+-- Question 10: Đếm số nhân viên thuộc department có id = 2
+SELECT DepartmentID,
+COUNT(AccountID) AS so_luong
+FROM `Account`
+WHERE DepartmentID=2;
+
+-- Question 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "D" và kết thúc bằng chữ "o"
+SELECT FullName
+FROM	`Account`
+WHERE	FullName LIKE 'D%o';
+
+-- Question 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
+DELETE
+FROM	Exam;
+
+-- Question 13: Xóa tất cả các question có nội dung bắt đầu bằng từ "câu hỏi"
+DELETE
+FROM	Question
+WHERE	Content='câu hỏi';
+
+-- Question 14: Update thông tin của account có id = 5 thành tên "Nguyễn Bá Lộc" và email thành loc.nguyenba@vti.com.vn
+UPDATE `Account`
+SET		FullName='Nguyen Ba Loc',
+		Email='thanhloc.nguyenba@vti.com.vn'
+WHERE	AccountID=5;
+
+-- Question 15: update account có id = 5 sẽ thuộc group có id = 4                   
+UPDATE GroupAccount 
+SET	AccountID=5
+WHERE GroupID=4;                    
